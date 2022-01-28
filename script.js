9b3df925988059b4b3d600c71a18894fc14552ff
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-01-28 16:17:54 +0300

    using methods and properties

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ rollback = 18;
 fullPrice = 80000;
 adaptive = false;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
