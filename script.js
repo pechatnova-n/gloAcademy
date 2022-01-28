7e040339bda76f06fd89cc1fdeb508b7cce58d8b
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-01-28 16:08:50 +0300

    value assignment

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let title, screens, screenPrice, rollback, fullPrice, adaptive;
 
 alert("Alert из lesson 1");
+console.log("Сообщение из lesson1");
 
-console.log("Сообщение из lesson1")
+title = "GLO";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 200;
+rollback = 18;
+fullPrice = 80000;
+adaptive = false;
+
+
