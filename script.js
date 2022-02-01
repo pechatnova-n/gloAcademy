40bbed142d0b3dbb146ca4383b2f1fe732398996
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-02 00:51:50 +0300

    adding calculation functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const title = prompt("Как называется ваш проект?");
+let title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать? (пример: 'Простые, Сложные, Интерактивные')");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
 const adaptive = confirm("Нужен ли адаптив на сайте?");
@@ -8,10 +8,12 @@ const service1 = prompt("Какой дополнительный тип услу
 const servicePrice1 = +prompt("Сколько это будет стоить?");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt("Сколько это будет стоить?");
-const rollback = 18;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let rollback = 10;
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+
 
 const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
@@ -29,22 +31,39 @@ const getRollbackMessage = function(price) {
     }
 }
 
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+}
+
+function getFullPrice() {
+    return screenPrice + allServicePrices;
+}
+
+const getTitle = function() {
+    title = title.trim().toLowerCase();
+    return title = title[0].toUpperCase() + title.slice(1);
+}
+
+function getServicePercentPrices(rollback) {
+    return fullPrice - (fullPrice * (rollback / 100));
+}
+
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices(rollback);
+getTitle();
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
+
+
 console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(servicePercentPrice);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
+console.log("Стоимость разработки сайта " + servicePercentPrice + " рублей");
+
+
 
 
 
